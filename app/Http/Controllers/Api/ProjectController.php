4d02a66f367fa5f0ce41651eb0118638f072e0fa
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-01 19:10:59 +0100

    [Fix] Small changes

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index()
     {
 
-        $projects = Project::with('type', 'technologies')->orderBy('id', 'desc')->paginate(6);
+        $projects = Project::all();
 
         return response()->json([
             'success' => true,
